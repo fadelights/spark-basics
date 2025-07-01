e9df8d39030e2c57d315eeda792892ab70eca49c
Mani <fadelights@aut.ac.ir>
2025-07-01 06:51:34 +0330

    name spark containers

--- sparkup.sh
+++ sparkup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 docker run --rm --interactive --tty \
+    --name sparkly \
     --publish 8888:8888 \
     --volume ~/Development/ipyspark:/home/mani/ipyspark \
     ipyspark:custom
